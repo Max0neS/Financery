5ab3f2bde5011704bb7431f935a12d8be99e6360
Максим <cromon541161@gmail.com>
2025-04-25 22:11:12 +0300

    First commit

---
+++ src/main/java/com/example/financery/FinanceryApplication.java
@@ -0,0 +1,13 @@
+package com.example.financery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/financery/FinanceryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
