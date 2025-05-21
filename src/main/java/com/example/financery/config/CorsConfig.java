2534582e0e4481dca6e496d34df932ddb1fb3471
Максим <cromon541161@gmail.com>
2025-05-21 23:29:34 +0300

    Sonar

--- src/main/java/com/example/financery/config/CorsConfig.java
+++ src/main/java/com/example/financery/config/CorsConfig.java
@@ -1,7 +1,5 @@
 package com.example.financery.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
